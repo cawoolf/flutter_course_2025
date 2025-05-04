24f6f49558928acf5db55584ec9b03d78616265d
cawoolf <destellodev@gmail.com>
2025-05-04 14:28:37 -0600

    166

--- meals_app/lib/widgets/meal_item.dart
+++ meals_app/lib/widgets/meal_item.dart
@@ -5,9 +5,10 @@ import 'package:transparent_image/transparent_image.dart';
 import '../models/meal.dart';
 
 class MealItem extends StatelessWidget {
-  const MealItem({super.key, required this.meal});
+  const MealItem({super.key, required this.meal, required this.onSelectMeal});
 
   final Meal meal;
+  final void Function(Meal meal) onSelectMeal;
   
   String get complexityText {
      return meal.complexity.name[0].toUpperCase() + meal.complexity.name.substring(1);
@@ -19,8 +20,6 @@ class MealItem extends StatelessWidget {
 
   }
 
-
-
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -29,7 +28,7 @@ class MealItem extends StatelessWidget {
       clipBehavior: Clip.hardEdge,
       elevation: 2,
       child: InkWell(
-        onTap: () {},
+        onTap: () {onSelectMeal(meal);},
         child: Stack(
           children: [
             FadeInImage(
