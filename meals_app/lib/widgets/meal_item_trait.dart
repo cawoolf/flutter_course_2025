92e566919669c008ddabb60cbd860f46cce823fd
cawoolf <destellodev@gmail.com>
2025-05-04 14:17:42 -0600

    166

---
+++ meals_app/lib/widgets/meal_item_trait.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class MealItemTrait extends StatelessWidget {
+  const MealItemTrait({super.key, required this.icon, required this.label});
+
+  final IconData icon;
+  final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Row(
+      children: [
+        Icon(icon, size: 17, color: Colors.white),
+        SizedBox(),
+        Text(
+          label,
+          style: TextStyle(color: Colors.white),
+        ),
+      ],
+    );
+  }
+}
