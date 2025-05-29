5324e0b8f1a51f3088e8dc89da97ed671cd0978a
cawoolf <destellodev@gmail.com>
2025-05-28 20:54:49 -0600

    167

--- meals_app/lib/screens/meal_details_screen.dart
+++ meals_app/lib/screens/meal_details_screen.dart
@@ -13,9 +13,49 @@ class MealDetailsScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(meal.title),
       ),
-      body: Image.network(meal.imageUrl,
-          width: double.infinity, height: 300, fit: BoxFit.cover),
+      body: SingleChildScrollView( // Ensures that content preserves formatting of child, but is Scrollable
+        child: Column(
+          children: [
+            _showMealPhoto(),
+            const SizedBox(height: 14),
+            _showTitle('Ingredients', context),
+            const SizedBox(height: 14),
+            for (final ingredient in meal.ingredients)
+              _showInstruction(ingredient, context),
+            const SizedBox(height: 24),
+            _showTitle('Steps', context),
+            for (final step in meal.steps)
+              Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+                  child: _showInstruction(step, context)),
+          ],
+        ),
+      ),
     );
+  }
+
+  Text _showTitle(String title, BuildContext context) {
+    return Text(
+      title,
+      style: Theme.of(context).textTheme.titleLarge!.copyWith(
+          color: Theme.of(context).colorScheme.primary,
+          fontWeight: FontWeight.bold),
+    );
+  }
+
+  Text _showInstruction(String instruction, BuildContext context) {
+    return Text(instruction,
+        textAlign: TextAlign.center,
+        style: Theme.of(context)
+            .textTheme
+            .bodyMedium!
+            .copyWith(color: Theme.of(context).colorScheme.onBackground));
+  }
+
+  Image _showMealPhoto() {
+    return Image.network(meal.imageUrl,
+        width: double.infinity, height: 300, fit: BoxFit.cover);
   }
 }
 
